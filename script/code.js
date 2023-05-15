2356846929a5c50ec27156e48bc9c172fdd6fc18
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-05-15 16:33:22 +0200

    first push

---
+++ script/code.js
@@ -0,0 +1,13 @@
+//console.log('hello world');
+//document.write('hello kwedini');
+
+// document.getElementById("para").innerText = "This is a paragraph";
+// document.querySelector("#para").textContent = "I love coding";
+
+// function content() {
+//     document.querySelector("#para").textContent  = "This is a prank";
+// }
+
+// let diplay = document.querySelector("#btn-displ");
+// diplay.addEventListener("click", content);
+
